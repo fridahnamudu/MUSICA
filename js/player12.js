d9781c85c98f45a992baf51cef1113078bb951c6
fridahnamudu <fridah.namudu@gmail.com>
2021-06-04 10:35:36 +0300

    add changes

--- js/player12.js
+++ js/player12.js
@@ -13,11 +13,346 @@ var ap1 = new APlayer({
     
     
     audio: [{
-      name: 'Ocean Eyes',
-      artist: 'Billie Ellish',
-      url: '/audio/billie.mp4',
-      cover: '/images/billie.jpeg'
-    }]
+      name: 'Lockdown',
+      artist: 'Koffee',
+      url: '/audio.reggae/Koffee - Lockdown _Official Video_.mp4',
+      cover: '/'
+    },
+
+    {
+      name: 'Babylon Feel Dis one',
+      artist: 'Bob Marley',
+      url: '/audio.reggae/Bob Marley - Babylon Feel Dis One _Take 2_.mp4',
+      cover: '/'
+    },
+    {
+      name: 'Dreams of brighter days',
+      artist: 'Busy Signal',
+      url: '/audio.reggae/Busy Signal _ RC - Dreams Of Brighter Days _Official Video_ - Prod. by Silly Walks Discotheque.mp4',
+      cover: '/'
+    },
+
+    
+    {
+      name: 'Ntty Dread',
+      artist: 'BOb Marley',
+      url: '/audio.reggae/Bob Marley - Natty Dread.mp4',
+      cover: '/'
+    },
+    {
+      name: 'Weekend love',
+      artist: 'Christopher Martin',
+      url: '/audio.reggae/Christopher Martin - Weekend Love _Official Video_.mp4',
+      cover: '/'
+    },
+    {
+      name: 'Skanking SWeet',
+      artist: 'Chronixx',
+      url: '/audio.reggae/Chronixx - _Skankin_ Sweet_ _Official Video_ _ Chronology.mp4',
+      cover: '/'
+    },
+
+    {
+      name: 'Zimbabwe',
+      artist: 'Bob Marley',
+      url: '/audio.reggae/Bob Marley - Zimbabwe.mp4',
+      cover: '/'
+    },
+    {
+      name: 'Pimpers Paradise',
+      artist: 'Bob Marley',
+      url: '/audio.reggae/Bob Marley Pimpers Paradise.mp4',
+      cover: '/'
+    },
+    {
+      name: 'Hills and valleys',
+      artist: 'Buju Banton',
+      url: '/audio.reggae/Buju Banton - Hills and valleys.mp4',
+      cover: '/'
+    },
+    {
+      name: 'JHey Dready',
+      artist: 'Burning Spear',
+      url: '/audio.reggae/Burning Spear - Hey Dready.mp4',
+      cover: '/'
+    },
+       {
+      name: 'Come over(missing you)',
+      artist: 'Busy Signal',
+      url: '/audio.reggae/Busy Signal - Come Over _Missing You_ _ Official Music Video.mp4',
+      cover: '/'
+    },
+    {
+      name: 'I wish you were here',
+      artist: 'Alpha Blondy',
+      url: '/audio.reggae/Alpha Blondy - I Wish You Were Here.mp4',
+      cover: '/'
+    },
+
+    {
+      name: 'ONe more night',
+      artist: 'Busy Signal',
+      url: '/audio.reggae/Busy Signal - Nightshift _ One More Night _ Official Music Video.mp4',
+      cover: '/'
+    },
+        {
+      name: 'Here comes Trouble',
+      artist: 'Chronixx',
+      url: '/audio.reggae/Chronixx - Here Comes Trouble _Official Music Video_.mp4',
+      cover: '/'
+    },
+    {
+      name: 'Smile Jamaica',
+      artist: 'Chronixx',
+      url: '/audio.reggae/Chronixx - Smile Jamaica _Official Video_ - prod. by Silly Walks Discotheque.mp4',
+      cover: '/'
+    },
+    {
+      name: 'Spanish Town Rockin',
+      artist: 'Chronixx',
+      url: '/audio.reggae/Chronixx - Spanish Town Rockin_ _OFFICIAL AUDIO_ _ Chronology.mp4',
+      cover: '/'
+    },
+    {
+      name: '18 and over',
+      artist: 'Cocoa tea',
+      url: '/audio.reggae/cocoa tea - 18 and over.mp4',
+      cover: '/'
+    },
+    {
+      name: 'LOve reggae',
+      artist: 'Collie Buddz',
+      url: '/audio.reggae/Collie Buddz - Love _ Reggae _Official Music Video_.mp4',
+      cover: '/'
+    },
+    {
+      name: 'They dont know',
+      artist: 'Chronixx',
+      url: '/audio.reggae/Chronixx - They Dont Know _Official Video_.mp4',
+      cover: '/'
+    },
+   
+    {
+      name: 'Blood A go run',
+      artist: 'culture',
+      url: '/audio.reggae/Culture - Blood A Go Run.mp4',
+      cover: '/'
+    },
+    {
+      name: 'Let love in',
+      artist: 'Dennis Brown',
+      url: '/audio.reggae/Dennis Brown  -  Let Love In.mp4',
+      cover: '/'
+    },
+    {
+      name: 'Zum Galli',
+      artist: 'Eek A Mouse',
+      url: '/audio.reggae/Eek-A-Mouse - Zum Galli(1).mp4',
+      cover: '/'
+    },
+    {
+      name: 'Cherry oh Baby',
+      artist: 'Eric Donaldson',
+      url: '/audio.reggae/Eric Donaldson - Cherry Oh Baby.mp4',
+      cover: '/'
+    },
+    {
+      name: 'True Rastaman',
+      artist: 'Fred Locks',
+      url: '/audio.reggae/Fred Locks - True Rastaman.mp4',
+      cover: '/'
+    },
+    {
+      name: 'Rockers Nu Crackers',
+      artist: 'Glen Washington',
+      url: '/audio.reggae/Glen Washington   Rockers Nu Crackers Inc Dub   Revival Dancehall.mp4',
+      cover: '/'
+    },
+    {
+      name: 'Feeling Kinda High',
+      artist: 'Gregory Isaac',
+      url: '/audio.reggae/Gregory Isaac - Feeling Kinda High.mp4',
+      cover: '/'
+    },
+    {
+      name: 'Night Nurse',
+      artist: 'Gregory Isaac',
+      url: '/audio.reggae/Gregory Isaacs - Night Nurse(8).mp4',
+      cover: '/'
+    },
+    {
+      name: 'African Child',
+      artist: 'Heptones',
+      url: '/audio.reggae/Heptones - African Child.mp4',
+      cover: '/'
+    },
+    {
+      name: 'Skylaking',
+      artist: 'Horace Andy',
+      url: '/audio.reggae/Horace Andy _Skylarking_.mp4',
+      cover: '/'
+    },
+    {
+      name: 'Ambush',
+      artist: 'Israel Vibration',
+      url: '/audio.reggae/Israel Vibration - Ambush.mp4',
+      cover: '/'
+    },
+    {
+      name: 'Jet plane',
+      artist: 'Sonia Spence',
+      url: '/audio.reggae/JET PLANE - SONIA SPENCE.mp4',
+      cover: '/'
+    },
+  
+    {
+      name: 'Vultures',
+      artist: 'Israel Vibration',
+      url: '/audio.reggae/Israel Vibration - Vultures.mp4',
+      cover: '/'
+    },
+    
+   
+    {
+      name: 'Toast',
+      artist: 'Koffee',
+      url: '/audio.reggae/Koffee - Toast _Official Video_.mp4',
+      cover: '/'
+    },
+    {
+      name: 'Cant believe',
+      artist: 'Kranium',
+      url: '/audio.reggae/Kranium - Can_t Believe ft. Ty Dolla _ign _ WizKid _Dance Video_.mp4',
+      cover: '/'
+    },
+    {
+      name: 'Natty Burial',
+      artist: 'Lone Ranger',
+      url: '/audio.reggae/Lone Ranger - Natty Burial.mp4',
+      cover: '/'
+    },
+    {
+      name: 'Who knows',
+      artist: 'Protoje',
+      url: '/audio.reggae/Protoje - Who Knows ft. Chronixx _Official Music Video_.mp4',
+      cover: '/'
+    },
+    {
+      name: 'Soul Provider',
+      artist: 'Romain Virgo',
+      url: '/audio.reggae/Romain Virgo - Soul Provider _Brighter Days Riddim_ - prod. by Silly Walks Discotheque.mp4',
+      cover: '/'
+    },
+    {
+      name: 'Unseen Guest',
+      artist: 'steel pulse',
+      url: '/audio.reggae/Steel Pulse - Unseen Guest.mp4',
+      cover: '/'
+    },
+    {
+      name: 'Love song',
+      artist: 'Etana',
+      url: '/audio.reggae/Etana - Love Song _Official Album Audio_.mp4',
+      cover: '/'
+    },
+
+    {
+      name: 'Cant cross the border',
+      artist: 'Sugar Minott',
+      url: '/audio.reggae/Sugar Minott - Can_t Cross The Border.mp4',
+      cover: '/'
+    },
+    {
+      name: 'Gimme likkle one drop',
+      artist: 'Tarrus Riley',
+      url: '/audio.reggae/Tarrus Riley - Gimme Likkle One Drop _Official Video_.mp4',
+      cover: '/'
+    },
+    {
+      name: 'Just the way you are',
+      artist: 'Tarrus Riley',
+      url: '/audio.reggae/Tarrus Riley - Just The Way You Are _Official Video_.mp4',
+      cover: '/'
+    },
+    {
+      name: 'Lighter',
+      artist: 'Tarrus Riley',
+      url: '/audio.reggae/Tarrus Riley - Lighter _Official Video_ ft. Shenseea_ Rvssian.mp4',
+      cover: '/'
+    },
+    {
+      name: 'Think Twice',
+      artist: 'Warriors Musick',
+      url: '/audio.reggae/Think Twice Riddim Video Medley_Studio Vibes_. Warriors Musick Prod..mp4',
+      cover: '/'
+    },
+    {
+      name: 'Colouring this life',
+      artist: 'Vybz Kartel',
+      url: '/audio.reggae/Vybz Kartel - Colouring This Life _Official Music Video_.mp4',
+      cover: '/'
+    },
+    {
+      name: 'STEP UP',
+      artist: 'IRIE LOVE',
+      url: '/audio.reggae/STEP UP - IRIE LOVE _Official Music Video_.mp4',
+      cover: '/'
+    },
+    {
+      name: 'Love song',
+      artist: 'Etana',
+      url: '/audio.reggae/Etana - Love Song _Official Album Audio_.mp4',
+      cover: '/'
+    },
+    {
+      name: 'Weakness in me',
+      artist: 'Etana',
+      url: '/audio.reggae/Etana - Weakness In Me.mp4',
+      cover: '/'
+    },
+    {
+      name: '',
+      artist: '',
+      url: '/',
+      cover: '/'
+    },
+    {
+      name: '',
+      artist: '',
+      url: '/',
+      cover: '/'
+    },
+    {
+      name: '',
+      artist: '',
+      url: '/',
+      cover: '/'
+    },
+    {
+      name: '',
+      artist: '',
+      url: '/',
+      cover: '/'
+    },
+    {
+      name: '',
+      artist: '',
+      url: '/',
+      cover: '/'
+    },
+    {
+      name: '',
+      artist: '',
+      url: '/',
+      cover: '/'
+    },
+
+    {
+      name: '',
+      artist: '',
+      url: '/',
+      cover: '/'
+}]
 
        
 });
--- js/player4.js
+++ js/player4.js
@@ -13,268 +13,457 @@ var ap1 = new APlayer({
     
     
     audio: [{
-      name: '',
-      artist: '',
-      url: '/',
+      name: 'All falls down',
+      artist: 'Alan Walker',
+      url: '/audio.edm/Alan Walker - All Falls Down _feat. Noah Cyrus with Digital Farm Animals_.mp4',
+      cover: '/'
+    },
+    {
+  
+      name: 'Alone',
+      artist: 'Alan Walker',
+      url: '/audio.edm/Alan Walker - Alone.webm',
+      cover: '/'
+    },
+    {
+  
+      name: 'Diampond Heart',
+      artist: 'Alan Walker',
+      url: '/audio.edm/Alan Walker - Diamond Heart _feat. Sophia Somajo_.mp4',
+      cover: '/'
+    },
+    {
+  
+      name: 'Heroes',
+      artist: 'Alesso',
+      url: '/audio.edm/Alesso - Heroes _we could be_ ft. Tove Lo.mp4',
+      cover: '/'
+    },
+    {
+  
+      name: 'Here Me Now',
+      artist: 'Alok _Bruno Martini',
+      url: '/audio.edm/Alok_ Bruno Martini feat. Zeeba - Hear Me Now _Official Music Video_.mp4',
+      cover: '/'
+    },
+    {
+  
+      name: 'Wasted',
+      artist: 'ALWZ SNNY',
+      url: '/audio.edm/ALWZ SNNY - Wasted ft. Krysta Youngs _Official Music Video_.webm',
+      cover: '/'
+    },
+    {
+  
+      name: 'Mrquette King',
+      artist: 'ALWZ SNNY',
+      url: '/audio.edm/ALWZ SNNY x Marquette King - SNNYLAND _Official Music Video_.webm',
+      cover: '/'
+    },
+    {
+  
+      name: 'My time',
+      artist: 'ALWZ SNNY',
+      url: '/audio.edm/ALWZ SNNY x Prezence - My Time _Official Music Video_.mp4',
+      cover: '/'
+    },
+    {
+  
+      name: 'Broken Arrows',
+      artist: 'Avicii',
+      url: '/audio.edm/Avicii - Broken Arrows.mp4',
+      cover: '/'
+    },
+    {
+  
+      name: ' The Nights',
+      artist: 'Avicii',
+      url: '/audio.edm/Avicii - The Nights.mp4',
+      cover: '/'
+    },
+    {
+  
+      name: 'Waiting for love',
+      artist: 'Avicii',
+      url: '/audio.edm/Avicii - Waiting For Love.webm',
+      cover: '/'
+    },
+    {
+  
+      name: 'Wake Me up',
+      artist: 'Avicii',
+      url: '/audio.edm/Avicii - Wake Me Up _Official Video_.webm',
+      cover: '/'
+    },
+    {
+  
+      name: 'Without YOu',
+      artist: 'Avicii',
+      url: '/audio.edm/Avicii - Without You _Part 4_ ft. Sandro Cavazza.mp4',
+      cover: '/'
+    },
+    {
+  
+      name: 'You make me',
+      artist: 'Avicii',
+      url: '/audio.edm/Avicii - You Make Me _Official_.mp4',
       cover: '/'
     },
     {
   
       name: '',
       artist: '',
-      url: '/',
+      url: '/audio.edm/Calvin Harris - Blame ft. John Newman.mp4',
       cover: '/'
     },
     {
   
-      name: '',
-      artist: '',
-      url: '/',
+      name: 'My Way',
+      artist: 'Calvin Harris',
+      url: '/audio.edm/Calvin Harris - My Way _Official Video_.mp4',
       cover: '/'
     },
     {
   
-      name: '',
-      artist: '',
-      url: '/',
+      name: 'Summer',
+      artist: 'Calvin Harris',
+      url: '/audio.edm/Calvin Harris - Summer _Official Video_.webm',
       cover: '/'
     },
     {
   
-      name: '',
-      artist: '',
-      url: '/',
+      name: 'This is what you came for',
+      artist: 'Calvin Harris',
+      url: '/audio.edm/Calvin Harris - This Is What You Came For _Official Video_ ft. Rihanna.webm',
       cover: '/'
     },
     {
   
-      name: '',
-      artist: '',
-      url: '/',
+      name: 'Giant',
+      artist: 'Calvin Harris',
+      url: '/audio.edm/Calvin Harris_ Rag_n_Bone Man - Giant _Official Video_.mp4',
       cover: '/'
     },
     {
   
-      name: '',
-      artist: '',
-      url: '/',
+      name: 'No Promises',
+      artist: 'Cheat Codes',
+      url: '/audio.edm/Cheat Codes - No Promises ft. Demi Lovato _Official Video_.mp4',
       cover: '/'
     },
     {
   
-      name: '',
-      artist: '',
-      url: '/',
+      name: 'Tears',
+      artist: 'Clean Bandit',
+      url: '/audio.edm/Clean Bandit -  Tears _feat. Louisa Johnson_ _Official Video_.mp4',
       cover: '/'
     },
     {
   
-      name: '',
-      artist: '',
-      url: '/',
+      name: 'Rockabye',
+      artist: 'Clean Bandit',
+      url: '/audio.edm/Clean Bandit - Rockabye _feat. Sean Paul _ Anne-Marie_ _Official Video_.webm',
       cover: '/'
     },
     {
   
-      name: '',
-      artist: '',
-      url: '/',
+      name: 'Solo',
+      artist: 'Clean Bandit',
+      url: '/audio.edm/Clean Bandit - Solo _feat. Demi Lovato_ _Official Video_.webm',
       cover: '/'
     },
     {
   
-      name: '',
-      artist: '',
-      url: '/',
+      name: 'Bad',
+      artist: 'Dvid Guetta',
+      url: '/audio.edm/David Guetta _ Showtek - Bad ft.Vassy _Lyrics Video_.webm',
       cover: '/'
     },
     {
   
-      name: '',
-      artist: '',
-      url: '/',
+      name: 'Hey mama',
+      artist: 'David Guetta',
+      url: '/audio.edm/David Guetta - Hey Mama _Official Video_ ft Nicki Minaj_ Bebe Rexha _ Afrojack.webm',
       cover: '/'
     },
     {
   
-      name: '',
-      artist: '',
-      url: '/',
+      name: 'LOvers on the sun',
+      artist: 'David Guetta',
+      url: '/audio.edm/David Guetta - Lovers On The Sun _Official Video_ ft Sam Martin.mp4',
       cover: '/'
     },
     {
   
-      name: '',
-      artist: '',
-      url: '/',
+      name: 'Titanium',
+      artist: 'David Guetta',
+      url: '/audio.edm/David Guetta - Titanium ft. Sia _Official Video_.webm',
       cover: '/'
     },
     {
   
-      name: '',
-      artist: '',
-      url: '/',
+      name: 'Without You',
+      artist: 'David Guetta',
+      url: '/audio.edm/David Guetta - Without You ft. Usher _Official Video_.mp4',
       cover: '/'
     },
     {
   
-      name: '',
-      artist: '',
-      url: '/',
+      name: 'This one is for you',
+      artist: 'David Guetta',
+      url: '/audio.edm/David Guetta ft. Zara Larsson - This One_s For You _Music Video_ _UEFA EURO 2016™ Official Song_.mp4',
       cover: '/'
     },
     {
   
-      name: '',
-      artist: '',
-      url: '/',
+      name: 'Five more hours',
+      artist: 'Deorro',
+      url: '/audio.edm/Deorro_ Chris Brown - Five More Hours _Official Video_ _Online Version_.mp4',
       cover: '/'
     },
     {
   
-      name: '',
-      artist: '',
-      url: '/',
+      name: 'Let me love you',
+      artist: 'DJ Snake',
+      url: '/audio.edm/DJ Snake - Let Me Love You ft. Justin Bieber _Official Video_.webm',
       cover: '/'
     },
     {
   
-      name: '',
-      artist: '',
-      url: '/',
+      name: 'MIddle',
+      artist: 'DJ Snake',
+      url: '/audio.edm/DJ Snake - Middle ft. Bipolar Sunshine.mp4',
       cover: '/'
     },
     {
   
-      name: '',
-      artist: '',
-      url: '/',
+      name: 'You know you like it',
+      artist: 'DJ Snake',
+      url: '/audio.edm/DJ Snake_ AlunaGeorge - You Know You Like It _Audio_.mp4',
       cover: '/'
     },
     {
   
-      name: '',
-      artist: '',
-      url: '/',
+      name: 'A Different Way',
+      artist: 'DJ Snake',
+      url: '/audio.edm/DJ Snake_ Lauv - A Different Way _Official Video_.mp4',
       cover: '/'
     },
     {
   
-      name: '',
-      artist: '',
-      url: '/',
+      name: 'All Day and Night',
+      artist: 'Europa_Jax Jones',
+      url: '/audio.edm/Europa _Jax Jones _ Martin Solveig_ - All Day and Night with Madison Beer _official video_.mp4',
       cover: '/'
     },
     {
   
-      name: '',
-      artist: '',
-      url: '/',
+      name: 'Only One',
+      artist: 'Felix Cartal',
+      url: '/audio.edm/Felix Cartal - Only One _ft. Karen Harding_ _Official Karaoke Video_.webm',
       cover: '/'
     },
     {
   
+      name: 'Aint nobody loves me better',
+      artist: 'Felix Jaehn',
+      url: '/audio.edm/Felix Jaehn - Ain_t Nobody _Loves Me Better_ _Official Video_ ft. Jasmine Thompson.mp4',
+      cover: '/'
+    },
+    {
+      name: 'Most Girls',
+      artist: 'Hailee Steinfeld',
+      url: '/audio.edm/Hailee Steinfeld - Most Girls _Official Video_.mp4',
+      cover: '/'
+    },
+    {
+      name: 'Starving',
+      artist: 'Hailee Steinfeld',
+      url: '/audio.edm/Hailee Steinfeld_ Grey - Starving ft. Zedd _Official Video_.mp4',
+      cover: '/'
+    },
+    {
+      name: 'You should be sad',
+      artist: 'Halsey',
+      url: '/audio.edm/Halsey - You should be sad _Tiësto Remix_.mp4',
+      cover: '/'
+    },
+    {
+      name: 'Beauty And A beast',
+      artist: 'Justine Beiber',
+      url: '/audio.edm/Justin Bieber - Beauty And A Beat ft. Nicki Minaj _Official Music Video_.webm',
+      cover: '/'
+    },
+    {
+      name: 'Die Young',
+      artist: 'Ke_ha',
+      url: '/audio.edm/Ke_ha - Die Young _Official Video_.mp4',
+      cover: '/'
+    },
+    {
+      name: 'Teddy Beats',
+      artist: 'Kitty',
+      url: '/audio.edm/Kitty - Mami _Teddy Beats Remix_ _Music Video_.mp4',
+      cover: '/'
+    },
+    {
+      name: '',
+      artist: '',
+      url: '/audio.edm/Klingande - Jubel _Official Video HD_.mp4',
+      cover: '/'
+    },
+    {
+      name: '',
+      artist: '',
+      url: '/audio.edm/Klingande - Jubel _Official Video HD_.mp4',
+      cover: '/'
+    },
+    {
+      name: '',
+      artist: '',
+      url: '/audio.edm/Kungs vs Cookin’ on 3 Burners - This Girl _Official Music Video_.mp4',
+      cover: '/'
+    },
+    {
+      name: '',
+      artist: '',
+      url: '/audio.edm/Kygo _ Whitney Houston - Higher Love _Official Video_.mp4',
+      cover: '/'
+    },
+    {
+      name: '',
+      artist: '',
+      url: '/audio.edm/Kygo - Happy Now ft. Sandro Cavazza _Official Video_.mp4',
+      cover: '/'
+    },
+    {
+      name: '',
+      artist: '',
+      url: '/audio.edm/Kygo - Stole The Show feat. Parson James _Official Music Video - YTMAs_.mp4',
+      cover: '/'
+    },
+    {
+      name: '',
+      artist: '',
+      url: '/audio.edm/Kygo_ Miguel - Remind Me to Forget _Official Video_.mp4',
+      cover: '/'
+    },
+    {
+      name: '',
+      artist: '',
+      url: '/audio.edm/Lost Frequencies - Are You With Me _Official Music Video_.mp4',
+      cover: '/'
+    },
+    {
+      name: '',
+      artist: '',
+      url: '/audio.edm/Lost Frequencies feat. Janieck Devy - Reality _Official Music Video_.mp4',
+      cover: '/'
+    },
+    {
+      name: '',
+      artist: '',
+      url: '/audio.edm/Major Lazer - Cold Water _feat. Justin Bieber _ MØ_ _Official Dance Video_.webm',
+      cover: '/'
+    },
+    {
+      name: '',
+      artist: '',
+      url: '/audio.edm/Major Lazer - Sua Cara _Feat. Anitta _ Pabllo Vittar_ _Official Music Video_.mp4',
+      cover: '/'
+    },
+    {
+      name: '',
+      artist: '',
+      url: '/audio.edm/Marshmello - Silence Ft. Khalid _Official Music Video_.mp4',
+      cover: '/'
+    },
+    {
+      name: '',
+      artist: '',
+      url: '/audio.edm/Marshmello ft. Bastille - Happier _Official Music Video_.mp4',
+      cover: '/'
+    },
+    {
+      name: '',
+      artist: '',
+      url: '/audio.edm/Martin Garrix _ David Guetta - So Far Away _Official Video_ feat. Jamie Scott _ Romy Dya.mp4',
+      cover: '/'
+    },
+    {
+      name: '',
+      artist: '',
+      url: '/audio.edm/Martin Garrix feat. Bonn - High On Life _Official Video_.mp4',
+      cover: '/'
+    },
+    {
+      name: '',
+      artist: '',
+      url: '/audio.edm/Mike Perry - The Ocean ft. Shy Martin.mp4',
+      cover: '/'
+    },
+    {
+      name: '',
+      artist: '',
+      url: '/audio.edm/Mike Posner - I Took A Pill In Ibiza _Seeb Remix_ _Explicit_.webm',
+      cover: '/'
+    },
+    {
+      name: '',
+      artist: '',
+      url: '/audio.edm/Mirours- Colorful Love _Official Music Video_.webm',
+      cover: '/'
+    },
+    {
+      name: '',
+      artist: '',
+      url: '/audio.edm/Nicki Minaj - Starships _Explicit_.mp4',
+      cover: '/'
+    },
+    {
+      name: '',
+      artist: '',
+      url: '/audio.edm/Njomza - over and done.webm',
+      cover: '/'
+    },
+    {
+      name: '',
+      artist: '',
+      url: '/audio.edm/Rain Radio _ DJ Craig Gorman - _Talk About_.mp4',
+      cover: '/'
+    },
+    {
+      name: '',
+      artist: '',
+      url: '/audio.edm/Rihanna - We Found Love ft. Calvin Harris.webm',
+      cover: '/'
+    },
+    {
+      name: '',
+      artist: '',
+      url: '/audio.edm/Rita Ora - Anywhere _Official Video_.mp4',
+      cover: '/'
+    },
+    {
+      name: '',
+      artist: '',
+      url: '/audio.edm/Robin Schulz - Sun Goes Down feat. Jasmine Thompson _Official Video_.mp4',
+      cover: '/'
+    },
+    {
       name: '',
       artist: '',
       url: '/',
       cover: '/'
     },
     {
-  
       name: '',
       artist: '',
       url: '/',
       cover: '/'
-    },
-    {
-  
-      name: '',
-      artist: '',
-      url: '/',
-      cover: '/'
-    },
-    {
-  
-      name: '',
-      artist: '',
-      url: '/',
-      cover: '/'
-    },
-    {
-  
-      name: '',
-      artist: '',
-      url: '/',
-      cover: '/'
-    },
-    {
-  
-      name: '',
-      artist: '',
-      url: '/',
-      cover: '/'
-    },
-    {
-  
-      name: '',
-      artist: '',
-      url: '/',
-      cover: '/'
-    },
-    {
-  
-      name: '',
-      artist: '',
-      url: '/',
-      cover: '/'
-    },
-    {
-  
-      name: '',
-      artist: '',
-      url: '/',
-      cover: '/'
-    },
-    {
-  
-      name: '',
-      artist: '',
-      url: '/',
-      cover: '/'
-    },
-    {
-  
-      name: '',
-      artist: '',
-      url: '/',
-      cover: '/'
-    },
-    {
-  
-      name: '',
-      artist: '',
-      url: '/',
-      cover: '/'
-    },
-    {
-  
-      name: '',
-      artist: '',
-      url: '/',
-      cover: '/'
-    },
-    {
-  
-      name: '',
-      artist: '',
-      url: '/',
-      cover: '/'
-    },
-    {
-      name: '',
-      artist: '',
-      url: '/',
-      cover: '/'
+    
+
+    
     
     }]
 
